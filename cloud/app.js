608e99cf836b44a0c490964d9d1b5c7b0ee1402b
dennis zhuang <killme2008@gmail.com>
2013-08-12 12:03:03 +0800

    add /isCool endpoint

--- cloud/app.js
+++ cloud/app.js
@@ -1,6 +1,7 @@
 // These two lines are required to initialize Express in Cloud Code.
 var express = require('express');
 var app = express();
+var name = require('cloud/name.js');
 
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
@@ -16,6 +17,12 @@ app.get('/hello', function(req, res) {
 app.post('/hello', function(req, res) {
   res.render('hello', { message: 'hello,' + req.body.name });
 });
+
+app.post('/isCool', function(req, res) {
+  var cool = name.isACoolName(req.body.name);
+  res.render('hello', { message: cool });
+});
+
 
 
 // This line is required to make Express respond to http requests.
