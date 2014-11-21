8c529ca87c64a0ff5043ec693a95439e998881b5
wchen <sdjcw13@gmail.com>
2014-11-21 18:59:12 +0800

    add testCase: cycle dependency

--- cloud/name.js
+++ cloud/name.js
@@ -2,3 +2,6 @@ var coolNames = ['Ralph', 'Skippy', 'Chip', 'Ned', 'Scooter'];
 exports.isACoolName = function(name) {
   return coolNames.indexOf(name) !== -1;
 }
+
+// 循环依赖测试 https://github.com/leancloud/cloud-code/issues/79
+require('cloud/main');
